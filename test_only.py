0d226d4954d2d1dee89f79f7ce25fed68330142b
caodoanh2001 <19521366@gm.uit.edu.vn>
2023-03-11 22:54:47 +0900

    update

--- test_only.py
+++ test_only.py
@@ -128,7 +128,7 @@ class Tester(Config):
         else:
             net = net_def.jl_efficientnet(task_mode=self.task_type.lower(), pretrained=True)
 
-        PATH_model = './log_colon_tma_20230311_att_ce_huber_rl/MULTI_ce_mse_cancer_Effi_seed5_BS64/_net_100.pth'
+        PATH_model = './log_colon_tma_20230311_att_ce_huber_rl/MULTI_ce_mse_cancer_Effi_seed5_BS64/_net_50.pth'
         net = torch.nn.DataParallel(net).to(device)
         checkpoint = torch.load(PATH_model)
         net.load_state_dict(checkpoint)
