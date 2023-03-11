585fa1dd564248529a12b89155bff1641fac8357
caodoanh2001 <19521366@gm.uit.edu.vn>
2023-03-11 23:02:55 +0900

    update

--- train_val_rl.py
+++ train_val_rl.py
@@ -71,8 +71,8 @@ class Trainer(Config):
         prob_baseline = F.softmax(logit_class_baseline, dim=-1)
         pred_baseline = torch.argmax(prob_baseline, dim=-1)
 
-        recall = f1_score(true.cpu().detach().numpy(), pred.cpu().detach().numpy(), average='macro')
-        recall_baseline = f1_score(true.cpu().detach().numpy(), pred_baseline.cpu().detach().numpy(), average='macro')
+        recall = recall_score(true.cpu().detach().numpy(), pred.cpu().detach().numpy(), average='macro')
+        recall_baseline = recall_score(true.cpu().detach().numpy(), pred_baseline.cpu().detach().numpy(), average='macro')
 
         loss_ = -torch.mean(prob, -1) * (recall - recall_baseline)
         loss_ = loss_.to(pred.device)
