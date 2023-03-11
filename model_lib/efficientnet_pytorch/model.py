a5a908916c168dcf5b4bbb3ceed693e44702fe05
caodoanh2001 <19521366@gm.uit.edu.vn>
2023-03-11 20:33:16 +0900

    update

--- model_lib/efficientnet_pytorch/model.py
+++ model_lib/efficientnet_pytorch/model.py
@@ -25,7 +25,6 @@ from model_lib.efficientnet_pytorch.utils import (
     MemoryEfficientSwish,
     calculate_output_image_size
 )
-import timm
 
 class MBConvBlock(nn.Module):
     """Mobile Inverted Residual Bottleneck Block.
