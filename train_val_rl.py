bd387998526b92fb77e2adc31a1b59eb45a819af
caodoanh2001 <19521366@gm.uit.edu.vn>
2023-03-11 22:37:24 +0900

    update

--- train_val_rl.py
+++ train_val_rl.py
@@ -200,7 +200,7 @@ class Trainer(Config):
         net = torch.nn.DataParallel(net).to(device)
 
         PATH_model = './_net_1950.pth'
-        net_baseline = net
+        net_baseline = net.copy()
         net_baseline = torch.nn.DataParallel(net_baseline).to(device)
         checkpoint = torch.load(PATH_model)
         net_baseline.load_state_dict(checkpoint)
