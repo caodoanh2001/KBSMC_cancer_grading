7f737f1a9b3f4280c2de4308c88ecd8e395262af
caodoanh2001 <19521366@gm.uit.edu.vn>
2023-03-12 11:41:36 +0900

    update

--- model_lib/efficientnet_pytorch/dcn.py
+++ model_lib/efficientnet_pytorch/dcn.py
@@ -29,16 +29,6 @@ class DeformableConv2d(nn.Module):
         nn.init.constant_(self.offset_conv.weight, 0.)
         nn.init.constant_(self.offset_conv.bias, 0.)
         
-        self.modulator_conv = nn.Conv2d(in_channels, 
-                                     1 * kernel_size[0] * kernel_size[1],
-                                     kernel_size=kernel_size, 
-                                     stride=stride,
-                                     padding=self.padding, 
-                                     bias=True)
-
-        nn.init.constant_(self.modulator_conv.weight, 0.)
-        nn.init.constant_(self.modulator_conv.bias, 0.)
-        
         self.regular_conv = nn.Conv2d(in_channels=in_channels,
                                       out_channels=out_channels,
                                       kernel_size=kernel_size,
@@ -51,14 +41,12 @@ class DeformableConv2d(nn.Module):
         #max_offset = max(h, w)/4.
 
         offset = self.offset_conv(x)#.clamp(-max_offset, max_offset)
-        modulator = 2. * torch.sigmoid(self.modulator_conv(x))
         
         x = torchvision.ops.deform_conv2d(input=x, 
                                           offset=offset, 
                                           weight=self.regular_conv.weight, 
                                           bias=self.regular_conv.bias, 
                                           padding=self.padding,
-                                          mask=modulator,
                                           stride=self.stride,
                                           )
         return x
